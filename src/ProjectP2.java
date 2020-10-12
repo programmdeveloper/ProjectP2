e680b24f49da1e954a483ecfee2204749131bbff
programmdeveloper <s0577603@student.htw-berlin.de>
2020-10-12 21:40:07 +0200

    Streams

--- src/ProjectP2.java
+++ src/ProjectP2.java
@@ -1,7 +1,48 @@
+import java.io.*;
+
 public class ProjectP2 {
 
+
+
     public static void main(String[] args) {
-       System.out.println("Hallo P2.");
+
+        //Schreiben in ein File -- FileOutputStream
+        String filename = "test.txt";
+        try {
+
+            OutputStream os = new FileOutputStream(filename);
+            String text = "Hallo HTW";
+            byte[] textAsByte = text.getBytes();
+
+            try {
+                os.write(textAsByte);
+            } catch (IOException ex){
+                System.err.println("couldn't write data");
+                System.exit(0);
+            }
+        } catch (FileNotFoundException ex) {
+           System.err.println("couldn't open file");
+           System.exit(0);
+        }
+
+
+
+        InputStream is = null;
+        try {
+            is = new FileInputStream(filename);
+        } catch (FileNotFoundException ex){
+            System.err.println("couldn't open file");
+            System.exit(0);
+        }
+        byte[] readBuffer = new byte[100];
+        try {
+            is.read(readBuffer);
+        } catch (IOException ex){
+            System.err.println("couldn't read String");
+            System.exit(0);
+        }
+        String readString = new String(readBuffer);
+        System.out.println("read something: " + readString);
     }
 
 }
