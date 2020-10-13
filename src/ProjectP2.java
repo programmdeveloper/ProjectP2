f2f98269fec7d384743bf8c12b15228fc3c7fe9b
programmdeveloper <s0577603@student.htw-berlin.de>
2020-10-13 14:01:22 +0200

    String in Streams

--- src/ProjectP2.java
+++ src/ProjectP2.java
@@ -14,16 +14,19 @@ public class ProjectP2 {
 
             os = new FileOutputStream(filename);
             dos = new DataOutputStream(os);
+
+            PrintStream ps = new PrintStream(os);
+            ps.println("Hello Stream");
             //String text = "Hallo HTW";
             //byte[] textAsByte = text.getBytes();
 
-            try {
+           /* try {
                 //os.write(textAsByte);
                 dos.writeInt(42);
             } catch (IOException ex){
                 System.err.println("couldn't write data");
                 System.exit(0);
-            }
+            } */
         } catch (FileNotFoundException ex) {
            System.err.println("couldn't open file");
            System.exit(0);
@@ -40,8 +43,8 @@ public class ProjectP2 {
             System.err.println("couldn't open file");
             System.exit(0);
         }
-        byte[] readBuffer = new byte[100];
-        /*try {
+        /*byte[] readBuffer = new byte[100];
+        try {
             is.read(readBuffer);
         } catch (IOException ex){
             System.err.println("couldn't read String");
@@ -49,15 +52,20 @@ public class ProjectP2 {
         }
         String readString = new String(readBuffer);
         System.out.println("read something: " + readString);*/
-        DataInputStream dis = new DataInputStream(is);
+        //DataInputStream dis = new DataInputStream(is);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String readString = null;
         int readIntValue = 0;
         try {
-            readIntValue = dis.readInt();
+            //readIntValue = dis.readInt();
+            readString = br.readLine();
+            System.out.println("read: " + readString);
         } catch (IOException ex){
             System.err.println("couldn't read data");
             System.exit(0);
         }
-        System.out.println("read Integer: " + readIntValue);
+        //System.out.println("read Integer: " + readIntValue);
     }
 
 }
