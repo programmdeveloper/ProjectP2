7608498de7c1f8f714da5a2f086a2db87c0b279d
programmdeveloper <s0577603@student.htw-berlin.de>
2020-10-13 15:28:56 +0200

    Tcp

--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -1,8 +1,6 @@
 package server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,19 +8,41 @@ public class TCPServer {
 
     public static void main(String[] args) {
 
-        String name = "localhost";
         int port = 3333;
-        try {
-            Socket socket = new Socket(name, port);
+        try{
             ServerSocket server = new ServerSocket(port);
-            socket = server.accept();
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
-        } catch (IOException ex){
-            System.err.println("connection failed");
-            System.exit(0);
+            System.out.println("Gestartet");
+
+            Socket client = server.accept();
+
+            InputStream is = client.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String receive = null;
+            while ((receive = br.readLine()) != null) {
+                System.out.println("Received: " + receive);
+            }
+
+            OutputStream os = client.getOutputStream();
+            PrintWriter x = new PrintWriter(os);
+            os.write("Hallo Welt".getBytes());
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException interruptedExceptionException){
+                System.out.println("fail");
+                System.exit(0);
+            }
+
+            client.close();
+            server.close();
+
         }
 
+
+        catch (IOException ex) {
+            System.err.println("fail");
+            System.exit(0);
+        }
 
     }
 
